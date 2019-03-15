562ce68cb763e3cc146cbabb0da1980dbe18cac1
macabuag <macabuag@gmail.com>
2019-03-15 20:29:46 +0000

    reads in required shapefiles and reads unique classes. ToDo: append these as a single vector of strings, reduce to a unique list and write to csv

--- scripts/OSM_Attribues.r
+++ scripts/OSM_Attribues.r
@@ -19,6 +19,11 @@ iFile$OSMfolder <- file.path("..", "..", "..", "Exposure", "OSM") #location of t
 oFolder <- list()
 
 ## User Inputs ##
+OSMs <- list()
+OSMs$filesUser <- c("gis_osm_buildings_a_free_1.shp", 
+                    "gis_osm_pois_a_free_1.shp",
+                    "gis_osm_pois_free_1.shp")
+
 
 ## Create Output Files ##
 oFolder$R <- paste0(gsub("[[:punct:]]", " ", Sys.time()), "-ExpDat")  #create a unique folder with the run date
@@ -39,10 +44,10 @@ if(!require("data.table")) {
 #   library(dplyr)
 # } # data manipulation
 # 
-# if(!require("rgdal")) {
-#   install.packages("rgdal")
-#   library(rgdal)
-# } # input/output, projections
+if(!require("rgdal")) {
+  install.packages("rgdal")
+  library(rgdal)
+} # input/output, projections
 # 
 # if(!require("sp")) {
 #   install.packages("sp")
@@ -63,4 +68,22 @@ if(!require("data.table")) {
 
 
 ## 2.0 Read Folder -----------------------------------------------
-OSMs <- list.files(iFile$OSMfolder, pattern = ".shp")
+OSMs <- list()
+OSMs$filesAll <- list.files(iFile$OSMfolder, pattern = ".shp")
+
+#a <- readOGR(dsn = file.path(iFile$OSMfolder, OSMs$filesUser[1]))
+
+OSMs$shp <- lapply(X = OSMs$filesUser, 
+                   FUN = function(x){readOGR(dsn = file.path(iFile$OSMfolder, x))})
+
+#ToDo: append these as a single vector of strings, reduce to a unique list and write to csv
+summary(OSMs$shp[[1]])
+head(OSMs$shp[[1]]@data)
+a <- unique(OSMs$shp[[1]]$type)
+
+head(OSMs$shp[[2]]@data)
+b <- unique(OSMs$shp[[2]]$fclass)
+
+c <- unique(OSMs$shp[[3]]$fclass)
+
+d <- unique(c(a,b,c))
